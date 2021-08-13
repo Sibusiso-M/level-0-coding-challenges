b09cef271f58ab57b2181132cdc7b543b2dd2132
Sibusiso-M <53113050+Sibusiso-M@users.noreply.github.com>
2021-08-13 13:03:54 +0200

    task 0.10 updates/changes

--- task-0.10.js
+++ task-0.10.js
@@ -1,9 +1,9 @@
 function commonLetters(string1, string2) {
     var matchingLetters = [];
-    for (var i = 0; i < string1.length; i++) { //loop through string1
-        for (var j = 0; j < string2.length; j++) { //loop through string2 with string1 as commparison
+    for (var i = 0; i < string1.length; i++) {
+        for (var j = 0; j < string2.length; j++) {
             if (string1[i] == string2[j]) {
-                if (matchingLetters.indexOf(string1[i]) == -1) { //check if matching letter/character in array matchingLetters
+                if (matchingLetters.indexOf(string1[i]) == -1) {
                     matchingLetters.push(string1[i]);
                 }
             }
