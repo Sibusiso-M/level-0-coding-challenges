174f97b6f9446ec9e9c67026c209758b606afa1d
Sibusiso-M <53113050+Sibusiso-M@users.noreply.github.com>
2021-09-17 13:38:15 +0200

    fix variable scope

--- task-0.8.js
+++ task-0.8.js
@@ -1,12 +1,12 @@
 function numberToTime(number) {
   if (number < 60) {
-    var minute = number;
+    let minute = number;
     return minute > 1 || minute == 0
       ? "0 hours, " + minute + " minutes"
       : "0 hours, " + minute + " minute";
   } else {
-    var minute = number % 60;
-    var hour = Math.floor(number / 60);
+    let minute = number % 60;
+    let hour = Math.floor(number / 60);
     return (
       (hour > 1 ? hour + " hours" : hour + " hour") +
       ", " +
