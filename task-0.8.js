f9c1de6c863a85b5072bf45df89564bbb61296f3
Sibusiso-M <53113050+Sibusiso-M@users.noreply.github.com>
2021-09-17 11:04:17 +0200

    task 0.8 formated

--- task-0.8.js
+++ task-0.8.js
@@ -1,14 +1,19 @@
- function numberToTime(number) {
-     if (number < 60) {
-         var minute = number;
-         return (minute > 1 || minute == 0) ? "0 hours, " + minute + " minutes" : "0 hours, " + minute + " minute";
-     } else {
-         var minute = (number % 60);
-         var hour = Math.floor(number / 60);
-         return ((hour > 1) ? hour + " hours" : hour + " hour") + ", " + ((minute > 1 || minute == 0) ? minute + " minutes" : minute + " minute");
-     }
- }
+function numberToTime(number) {
+  if (number < 60) {
+    var minute = number;
+    return minute > 1 || minute == 0
+      ? "0 hours, " + minute + " minutes"
+      : "0 hours, " + minute + " minute";
+  } else {
+    var minute = number % 60;
+    var hour = Math.floor(number / 60);
+    return (
+      (hour > 1 ? hour + " hours" : hour + " hour") +
+      ", " +
+      (minute > 1 || minute == 0 ? minute + " minutes" : minute + " minute")
+    );
+  }
+}
 
- //Example
- console.log(numberToTime(133));
- console.log(numberToTime(0));
+console.log(numberToTime(133));
+console.log(numberToTime(0));
