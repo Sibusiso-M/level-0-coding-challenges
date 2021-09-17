e33501e1241d1b0aba30c92f11d87d99e02f7325
Sibusiso-M <53113050+Sibusiso-M@users.noreply.github.com>
2021-09-17 11:04:54 +0200

    task 0.10 formated

--- task-0.10.js
+++ task-0.10.js
@@ -1,14 +1,14 @@
 function commonLetters(string1, string2) {
-    var matchingLetters = [];
-    for (var i = 0; i < string1.length; i++) {
-        for (var j = 0; j < string2.length; j++) {
-            if (string1[i] == string2[j]) {
-                if (matchingLetters.indexOf(string1[i]) == -1) {
-                    matchingLetters.push(string1[i]);
-                }
-            }
+  var matchingLetters = [];
+  for (var i = 0; i < string1.length; i++) {
+    for (var j = 0; j < string2.length; j++) {
+      if (string1[i] == string2[j]) {
+        if (matchingLetters.indexOf(string1[i]) == -1) {
+          matchingLetters.push(string1[i]);
         }
+      }
     }
-    console.log("Common letters : " + matchingLetters);
+  }
+  console.log("Common letters : " + matchingLetters);
 }
 commonLetters("string1", "stttring21");
