6c3c154e1027981c3f0390839107ee9a7a007e7f
Sibusiso-M <53113050+Sibusiso-M@users.noreply.github.com>
2021-09-17 11:02:05 +0200

    task 0.6 formated

--- task-0.6.js
+++ task-0.6.js
@@ -1,17 +1,15 @@
- //bonus question attempt
- function getMaximum() {
+//bonus question attempt
+function getMaximum() {
+  var max = arguments[0];
 
-     var max = arguments[0];
+  for (index = 0; index < arguments.length; index++) {
+    if (max < arguments[index]) {
+      max = arguments[index];
+    }
+  }
 
-     for (index = 0; index < arguments.length; index++) {
+  return max;
+}
 
-         if (max < arguments[index]) {
-             max = arguments[index];
-         }
-     }
-
-     return max;
- }
-
- console.log(getMaximum(100, 88, 9, 99, 0, 7, 600));
- console.log(getMaximum(-100, -88, -9, -99, 0, -7, -600));
+console.log(getMaximum(100, 88, 9, 99, 0, 7, 600));
+console.log(getMaximum(-100, -88, -9, -99, 0, -7, -600));
