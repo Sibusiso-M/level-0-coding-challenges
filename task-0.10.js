a8e1e67bc3b4c71524c1e9cf4205fb54641c3368
Sibusiso-M <53113050+Sibusiso-M@users.noreply.github.com>
2021-10-01 00:12:51 +0200

    changed code style

--- task-0.10.js
+++ task-0.10.js
@@ -1,14 +1,11 @@
-function commonLetters(string1, string2) {
-    let matchingLetters = [];
-    for (let i = 0; i < string1.length; i++) {
-        for (let j = 0; j < string2.length; j++) {
-            if (string1[i] == string2[j]) {
-                if (matchingLetters.indexOf(string1[i]) == -1) {
-                    matchingLetters.push(string1[i]);
-                }
-            }
-        }
+function commonLetters2(string1, string2) {
+  let matchingLetters = [];
+  for (let i = 0; i < string1.length; i++) {
+    if (string2.indexOf(string1[1])) {
+      matchingLetters.push(string1[i]);
     }
-    console.log("Common letters : " + matchingLetters);
+  }
+  console.log("common letters: " + matchingLetters);
 }
-commonLetters("string1", "stttring21");
+
+commonLetters2("string1", "stttring21");
