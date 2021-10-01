2e05e490def45d3965bd485785f891f1bb7c5e01
Sibusiso-M <53113050+Sibusiso-M@users.noreply.github.com>
2021-10-01 17:04:32 +0200

    code logic correction

--- task-0.10.js
+++ task-0.10.js
@@ -1,11 +1,13 @@
-function commonLetters2(string1, string2) {
+function commonLetters(string1, string2) {
   let matchingLetters = [];
   for (let i = 0; i < string1.length; i++) {
-    if (string2.indexOf(string1[1])) {
+    if (string2.indexOf(string1[i]) !== -1) {
       matchingLetters.push(string1[i]);
+      console.log(i);
     }
   }
   console.log("common letters: " + matchingLetters);
 }
 
-commonLetters2("string1", "stttring21");
+commonLetters("string1", "stttring21");
+commonLetters("house", "computers");
